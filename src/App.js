1671650a96d61648c253e0a923fc08f1cbe4a464
FaabScheeren <f.scheeren@outlook.com>
2020-02-24 12:14:35 +0100

    First own build component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+function Header() {
+  return (
+    <h1>Welcome to Codaisseur</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to Codaisseur</h2>
+      <Header />
     </div>
   );
 }
